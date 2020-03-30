be169386734d2078caa732f2ac3526ef4d470962
adnanali99 <aliadnan38@gmail.com>
2020-03-30 18:13:19 +0500

    Created Test Suites and then Test Suits collection

---
+++ Test Suites/ChiraghWeb_FrontEnd_SmokeTests.ts
@@ -0,0 +1,43 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>ChiraghWeb_FrontEnd_SmokeTests</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>ba949c3b-4b9c-40bc-98d0-4593ce175822</testSuiteGuid>
+   <testCaseLink>
+      <guid>5c6ea244-e28a-4ff4-abcb-f2cab83a25e8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Front End/TC_LoginWithValidCredentials_001</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>08b1bc78-b44b-42f8-90ee-d0b4c11cdeff</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Front End/TC_SmokeTests_006</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>914ca2e6-3aa7-4321-aa02-c24bce277207</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Smoke Tests/Header/TC_VerifyBuyTab_008</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8f19b848-7ac6-4c79-b052-27d2084561f4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Smoke Tests/Header/TC_VerifyRentTab_010</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>43886122-3e9a-4a84-a70f-2003e558d858</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Smoke Tests/Header/TC_VerifySellTab_009</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
---
+++ Test Suites/ChiraghWeb_FrontEnd_TopUps.ts
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>This Test suit will perform all the top ups on Chiragh website</description>
+   <name>ChiraghWeb_FrontEnd_TopUps</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>6340a1bd-4b8e-4786-b51f-7b9e56542c31</testSuiteGuid>
+   <testCaseLink>
+      <guid>09475da5-d511-4c8d-84d2-47a81fdd8c21</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Front End/TC_TopUP(AED)_003</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e166b7b2-a99a-4cde-b50e-a6a1fa8ed89b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Front End/TC_TopUP(EUR)_004</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4c9369c5-cb21-4b28-8160-a7129555077f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Front End/TC_TopUP(USD)_005</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
