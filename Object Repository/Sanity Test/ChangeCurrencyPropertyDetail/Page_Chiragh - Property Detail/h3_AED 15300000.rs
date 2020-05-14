ac10f8fc33fc71514283e95406d051f0f517ff80
ahsankalim <ahsan@bestercapitalmedia.com>
2020-05-14 14:35:10 +0500

    implement the behavior of my account fields

---
+++ Object Repository/Sanity Test/ChangeCurrencyPropertyDetail/Page_Chiragh - Property Detail/h3_AED 15300000.rs
@@ -0,0 +1,85 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>h3_AED 15300000</name>
+   <tag></tag>
+   <elementGuidId>27cf2350-d7d2-4177-a7cb-ecdb5333afce</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>(.//*[normalize-space(text()) and normalize-space(.)='AUCTION OPEN PRICE:'])[1]/following::h3[1]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>h3</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>h3-title-22 m-0</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value> AED: 153,000.00 </value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[@class=&quot;fontawesome-i2svg-active fontawesome-i2svg-pending&quot;]/body[@class=&quot;modal-main-open&quot;]/app-root[1]/app-core[1]/app-property-detail[1]/section[1]/div[@class=&quot;container-fluid&quot;]/div[@class=&quot;property-detail-view&quot;]/div[@class=&quot;row&quot;]/div[@class=&quot;col-lg-4&quot;]/app-property-bidding[1]/div[@class=&quot;bid-now&quot;]/div[@class=&quot;bid-component&quot;]/div[@class=&quot;bid-first-body-section&quot;]/div[@class=&quot;bidaucitonOuter&quot;]/div[@class=&quot;bidAuctionInner&quot;]/h3[@class=&quot;h3-title-22 m-0&quot;]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='AUCTION OPEN PRICE:'])[1]/following::h3[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Reserve price met'])[1]/following::h3[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='AED'])[1]/preceding::h3[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='CURRENT BID'])[1]/preceding::h3[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='AED: 153,000.00']/parent::*</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[2]/div/div/h3</value>
+   </webElementXpaths>
+</WebElementEntity>
--- Scripts/Sanity Tests/Buy_Bid Now_ChangeCurrency_12/Script1589437412241.groovy
+++ Scripts/Sanity Tests/Buy_Bid Now_ChangeCurrency_12/Script1589437412241.groovy
@@ -14,4 +14,39 @@ import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('http://test.chiragh.com/')
+
+WebUI.click(findTestObject('Object Repository/Sanity Test/ChangeCurrencyPropertyDetail/Page_Chiragh - Home/a_ENG_dropdownMenuLink'))
+
+WebUI.click(findTestObject('Object Repository/Sanity Test/ChangeCurrencyPropertyDetail/Page_Chiragh - Home/a_Login'))
+
+WebUI.setText(findTestObject('Object Repository/Sanity Test/ChangeCurrencyPropertyDetail/Page_Chiragh - Login/input_Username_un'), 
+    'AUTOCHIRAGH')
+
+WebUI.setEncryptedText(findTestObject('Object Repository/Sanity Test/ChangeCurrencyPropertyDetail/Page_Chiragh - Login/input_Password_userPassword'), 
+    'RigbBhfdqOBDK95asqKeHw==')
+
+WebUI.click(findTestObject('Object Repository/Sanity Test/ChangeCurrencyPropertyDetail/Page_Chiragh - Login/button_Log In'))
+
+
+WebUI.click(findTestObject('Object Repository/Sanity Test/ChangeCurrencyPropertyDetail/Page_Chiragh - Home/button_Bid Now'))
+
+//WebUI.click(findTestObject('Object Repository/Sanity Test/ChangeCurrencyPropertyDetail/Page_Chiragh - Property Detail/div_AED_ng-input'))
+
+WebUI.delay(10)
+
+WebUI.click(findTestObject('Object Repository/Sanity Test/ChangeCurrencyPropertyDetail/Page_Chiragh - Property Detail/div_AED_ng-input'))
+
+WebUI.delay(4)
+
+WebUI.selectOptionByValue(findTestObject('Object Repository/Sanity Test/ChangeCurrencyPropertyDetail/Page_Chiragh - Property Detail/div_AED_ng-input'),
+	'1', true)
+
+WebUI.click(findTestObject('Object Repository/Sanity Test/ChangeCurrencyPropertyDetail/Page_Chiragh - Property Detail/h3_AED 15300000'))
+
+WebUI.closeBrowser()
 
