16fa2267ffd0f532fb97a52ede71ce1c558197a1
ahsankalim <ahsan@bestercapitalmedia.com>
2020-05-20 12:39:06 +0500

    implement the Buy Property Bidding "Register Now" Button for New user to force him for register first.

---
+++ Object Repository/Sanity Test/PropertyBidingRegistration/Page_Chiragh - Property Detail/div_Log InOR Register.rs
@@ -0,0 +1,57 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_Log InOR Register</name>
+   <tag></tag>
+   <elementGuidId>c87acdc0-845d-4883-82eb-c7b4532bb1a7</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>(.//*[normalize-space(text()) and normalize-space(.)='BID NOW'])[1]/following::div[3]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>login-div</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Log InOR Register</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[@class=&quot;fontawesome-i2svg-active fontawesome-i2svg-pending&quot;]/body[@class=&quot;modal-main-open&quot;]/app-root[1]/app-core[1]/app-property-detail[1]/section[1]/div[@class=&quot;container-fluid&quot;]/div[@class=&quot;property-detail-view&quot;]/div[@class=&quot;row&quot;]/div[@class=&quot;col-lg-4&quot;]/app-property-bidding[1]/div[@class=&quot;bid-now&quot;]/div[@class=&quot;bid-component&quot;]/div[@class=&quot;bid-submit-block&quot;]/div[@class=&quot;bid-now-wrapper h-100&quot;]/div[@class=&quot;login-tooltip&quot;]/div[@class=&quot;login-div&quot;]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='BID NOW'])[1]/following::div[3]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[4]/div/div/div[2]</value>
+   </webElementXpaths>
+</WebElementEntity>
